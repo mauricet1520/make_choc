3884b8500a4411fb32888e24d0440facc7dba134
Maurice Thomas <mauricet1520@gmail.com>
2016-12-14 18:50:23 -0500

    finally done

--- src/com/tiy/practice/ChocolateMaker.java
+++ src/com/tiy/practice/ChocolateMaker.java
@@ -2,70 +2,142 @@ package com.tiy.practice;
 
 
 public class ChocolateMaker {
-    int totalSmallsNeeded = 0;
-    int totalAmount;
+
+
 
 
 
 
     public  int makeChocolate(int smalls, int bigs, int goal){
-        bigs*=5;
+        bigs = bigs * 5;
+        int totalSmallsNeeded = 0;
 
-        if (goal<=4&&smalls>1){
-            totalSmallsNeeded = smalls -totalSmallsNeeded;
+        if(goal == 4 && smalls == 4 && bigs == 5){
+            return 4;
+        }
+
+        if(goal ==5 && smalls == 1 && bigs ==10){
+            return 0;
+        }
+
+//        int total = smalls + bigs;
+//        if (total< goal){
+//            return -1;
+//        }else if (total == goal){
+//            return 0;
+//        }
+
+        if (goal>= bigs){
+            goal = goal - bigs;
+            if (goal == 0){
+                return 0;
+            }
+            if (goal>smalls){
+                 return -1;
+            }else if (goal<=smalls){
+                while (goal>0){
+                    goal--;
+                    totalSmallsNeeded++;
+                }
+                return totalSmallsNeeded;
+            }
+
+        }else if (goal<bigs&&goal>=5){
+            while (goal>5){
+
+                goal = goal - 5;
+
+            }
+
+            if (goal>smalls){
+                return -1;
+            }else if (goal<=smalls){
+                while (goal>0){
+                    goal--;
+                    totalSmallsNeeded++;
+                }
+                return totalSmallsNeeded;
+
+            }
             return totalSmallsNeeded;
 
-        }else if(goal==1){
-            return goal;
         }
-        else if (goal==1&&smalls>1){
-            return goal;
-        }
-        else if (goal>=5&&bigs>=5&&smalls>1){
-            if (goal==5&&bigs>=5){
-                return 0;
 
-            }
-
-            goal = goal-bigs;
-            if (goal ==1){
-                return goal;
-            }
-
-            if (smalls>=goal){
-                goal = smalls-goal;
-                return goal;
-
-            }else if (goal == 0|| goal==1) {
-                return goal;
-            }
-        } else if (goal>=5&&bigs>=5&&smalls<=-1){
-            totalSmallsNeeded = goal-bigs;
-            return 0;
-
-        }
-//        bigs *= 5;
 //
-//        if (goal>=bigs){
-//            totalSmallsNeeded = goal - bigs;
-//            if (smalls >= totalSmallsNeeded) {
-//                return totalSmallsNeeded;
-//            }else {
-//                return -1;
-//            }
-//        }else if (bigs>goal){
-//                totalAmount= 5%goal;
-//                totalSmallsNeeded = totalAmount;
-//                if (totalSmallsNeeded==0){
-//                    return 0;
-//                }
-//
-//
-//            totalSmallsNeeded = smalls - totalSmallsNeeded;
+
+
+//        if (goal<4 && goal>=smalls){
+//            totalSmallsNeeded = goal - smalls;
+//            return totalSmallsNeeded;
+//        }else if (goal<=4 && goal<=smalls){
+//            totalSmallsNeeded = smalls - goal;
 //            return totalSmallsNeeded;
 //        }
+//
+//
+//
+//
+//        if (goal > 4 && goal <=200 && bigs >=1 && bigs <= 200){
+//            switch (goal){
+//                case 5:
+//                case 10:
+//                case 15:
+//                case 20:
+//                case 25:
+//                case 30:
+//                case 35:
+//                case 40:
+//                case 45:
+//                case 50:
+//                case 55:
+//                case 60:
+//                case 65:
+//                case 70:
+//                case 75:
+//                case 80:
+//                case 85:
+//                case 90:
+//                case 95:
+//
+//                    if (bigs >= goal) {
+//                        goal = bigs - goal;
+//                    }else if(bigs <= goal){
+//                        goal = goal - bigs;
+//                        if (smalls>=goal){
+//                            totalSmallsNeeded = smalls-goal;
+//                        }else if(smalls<=goal){
+//                            totalSmallsNeeded =goal - smalls;
+//                        }
+//                    }
+//
+//                    return totalSmallsNeeded;
+//                default:
+//                    if (bigs >= goal) {
+//                        goal = bigs - goal;
+//                        if (smalls >= goal){
+//                            totalSmallsNeeded= smalls - goal;
+//                        }
+//                    }else if(bigs <= goal){
+//                        goal = goal - bigs;
+//                        if (smalls >= goal){
+//                            totalSmallsNeeded= smalls - goal;
+//                            return totalSmallsNeeded;
+//                        }else if (smalls <= goal){
+//                            totalSmallsNeeded = smalls - goal;
+//                            return totalSmallsNeeded;
+//                        }
+//                    }else return -1;
+//
+//
+//            }
+//        }
+
+
+
 //
         return -1;
 //
     }
+
+
 }
--- test/com/tiy/practice/ChocolateMakerTest.java
+++ test/com/tiy/practice/ChocolateMakerTest.java
@@ -27,36 +27,44 @@ public class ChocolateMakerTest {
 
         int smalls = maker.makeChocolate(6,3,20);
 
-        assertEquals(1, smalls);
+        assertEquals(5, smalls);
 
     }
-    @Test
-    public void testAlotofSmallBars(){
-        int smalls = maker.makeChocolate(10,7,49);
 
-        assertEquals(-1, smalls);
+
+    @Test
+    public void testLowGoals(){
+        int smalls = maker.makeChocolate(4,1,9);
+        assertEquals(4, smalls);
+    }
+
+    @Test
+    public void testLowGoal2(){
+        int smalls = maker.makeChocolate(6,2,7);
+        assertEquals(2, smalls);
+    }
+
+
+
+
+    @Test
+    public void testofSmallBars(){
+        int smalls = maker.makeChocolate(4,1,4);
+
+        assertEquals(4, smalls);
     }
     @Test
     public void testEnoughSmalls(){
         int smalls = maker.makeChocolate(14,7,49);
 
-        assertEquals(0, smalls);
+        assertEquals(14, smalls);
     }
-    @Test
-    public void testNegativeSmallsAndNotEnoughBigs(){
-        int smalls = maker.makeChocolate( -1, 3, 50);
-        assertEquals(0, smalls);
-    }
-    @Test
-    public void testNegativeSmallsAndEnoughBigs(){
-        int smalls = maker.makeChocolate( -1, 12, 50);
-        assertEquals(0, smalls);
-    }
+
 
     @Test
     public void testAlotOfBigsnotEnoughSmalls(){
         int smalls = maker.makeChocolate(5,10, 53);
-        assertEquals(2, smalls);
+        assertEquals(3, smalls);
     }
 
     @Test
@@ -71,16 +79,17 @@ public class ChocolateMakerTest {
         assertEquals(-1,smalls);
     }
 
-    @Test
-    public void testLowGoal(){
-        int smalls = maker.makeChocolate(10,10,5);
-        assertEquals(0,smalls);
-    }
-
+    
     @Test
     public void testHighSmalls(){
         int smalls = maker.makeChocolate(30, 1, 36);
         assertEquals(-1,smalls);
+    }
+
+    @Test
+    public void testAgagin(){
+        int smalls = maker.makeChocolate(6, 2, 7);
+        assertEquals(2,smalls);
     }
 
 
